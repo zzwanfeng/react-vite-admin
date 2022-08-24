2e8c9ff240370064ba54281e71342a9c4b528845
zzh <1095959430@qq.com>
2022-08-24 22:16:13 +0800

    基础开发

---
+++ README.md
@@ -0,0 +1,30 @@
+## 简介
+
+**react-vite-admin**
+
+基于 React 生态系统搭建的后台管理系统模板
+
+## 技术栈
+
+react@16.10.1 + vite@3.0.7 + antd@3.23.5 + axios@0.19.0
+
+## 快速启动
+
+```
+# 克隆项目
+git clone git@github.com:zzhStrive/react-vite-admin.git
+
+
+
+# 进入项目目录
+cd react-vite-admin
+
+# 安装依赖
+npm install  / yarn
+
+# 启动服务
+npm run dev  / yarn dev
+
+# 打包
+npm run build  / yarn build
+```
--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,18 +1,90 @@
 import React from 'react'
-import { Layout, Divider } from 'antd'
+import { Layout, Divider, Row, Descriptions, Badge } from 'antd'
 import CustomBreadcrumb from '@/components/CustomBreadcrumb'
+import ProjectConfigJson from '../../../package.json'
 
-const AboutView = () => (
+
+const { dependencies, devDependencies } = ProjectConfigJson
+const DependenciesArr = []
+const DevDependenciesArr = []
+
+Object.keys(dependencies).forEach(key => {
+  const dependenciesObj = { name: '', version: '' }
+  dependenciesObj.name = key
+  dependenciesObj.version = dependencies[key]
+  DependenciesArr.push(dependenciesObj)
+})
+
+Object.keys(devDependencies).forEach(key => {
+  const dependenciesObj = { name: '', version: '' }
+  dependenciesObj.name = key
+  dependenciesObj.version = devDependencies[key]
+  DevDependenciesArr.push(dependenciesObj)
+})
+console.log('DependenciesArr', DependenciesArr);
+console.log('DevDependenciesArr', DevDependenciesArr);
+
+
+const DependenciesView = () => {
+  return (
+    <Descriptions
+      title="生产环境依赖"
+      bordered
+      column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+    >
+      {
+        DependenciesArr && DependenciesArr.map((item, index) => {
+          return (
+            <Descriptions.Item label={item.name} key={`'Dep' + ${index}`} > {item.version}</Descriptions.Item>
+          )
+        })
+      }
+    </Descriptions>
+  )
+}
+
+const DevDependenciesView = () => {
+  return (
+    <Descriptions
+      title="开发环境依赖"
+      bordered
+      column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+    >
+      {
+        DevDependenciesArr && DevDependenciesArr.map((item, index) => {
+          return (
+            <Descriptions.Item label={item.name} key={`'Dep' + ${index}`} > {item.version}</Descriptions.Item>
+          )
+        })
+      }
+    </Descriptions>
+  )
+}
+
+
+const AboutView = () => {
+
+  return (
     <Layout>
-        <div>
-            <CustomBreadcrumb arr={['关于']}></CustomBreadcrumb>
-        </div>
-        <div className='base-style'>
-            <h3>关于作者</h3>
-            <Divider />
-            <p>这个人很懒，什么都没有留下……</p>
-        </div>
+      <div>
+        <CustomBreadcrumb arr={['关于']}></CustomBreadcrumb>
+      </div>
+
+      <div className='base-style'>
+        <h3>关于作者</h3>
+        <Divider />
+        <p>这个人很懒，什么都没有留下……</p>
+      </div>
+
+      <div className='base-style'>
+        <DependenciesView></DependenciesView>
+      </div>
+
+      <div className='base-style'>
+        <DevDependenciesView></DevDependenciesView>
+      </div>
     </Layout>
-)
+  )
+}
 
 export default AboutView
