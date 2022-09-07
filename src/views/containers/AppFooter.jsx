1a6576a155e0352b8e327fee7d85d1c1b2247d6d
zzh <1095959430@qq.com>
2022-09-07 18:06:21 +0800

    修改

--- src/views/containers/AppFooter.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Layout } from 'antd'
-
-const { Footer } = Layout
-
-export default () => <Footer className='footer'>React Admin &copy;2022 Created By zzhStrive</Footer>
