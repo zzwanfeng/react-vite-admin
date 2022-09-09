acb3400e658790d36128d68f7678b2d7455b39a1
zzh <1095959430@qq.com>
2022-09-09 11:02:50 +0800

    开发

--- src/components/PageLoading/Index.jsx
+++ src/components/PageLoading/Index.jsx
@@ -1,43 +1,16 @@
-// import React from 'react'
-// import loading1 from '@/assets/images/loading1.gif'
+import React from 'react'
+import loading1 from '@/assets/images/loading1.gif'
 
 
-// const PageLoading = () => {
-//   return (
-//     <div className='r-flex aic jsc'>
-//       <img src={loading1} alt="" />
-//     </div>
-//   )
-// }
-
-
-
-
-// export default PageLoading
-
-
-import { FC, useEffect, useState } from 'react';
-// import ContentLoader from './content-loader'; // or any spinner component
-
-const aa = () => {
+const PageLoading = () => {
   return (
-    <div>123</div>
+    <div className='r-flex aic jsc'>
+      <img src={loading1} alt="" />
+    </div>
   )
 }
 
-const LazyLoader = ({ delay = 250, ...props }) => {
-  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShow(true);
-    }, delay);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [delay]);
 
-  return show ? <aa /> : null;
-};
 
-export { LazyLoader as default };
+export default PageLoading
